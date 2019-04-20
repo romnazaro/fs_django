dc229921afbe9458331d06f94d6962c61b5e57fb
Roman Nazarov <romnazaro@gmail.com>
2019-04-20 19:24:51 +0300

    Output moderated comments

--- post/views.py
+++ post/views.py
@@ -1,11 +1,27 @@
-from django.shortcuts import render, get_object_or_404
-from post.models import Post
+from django.shortcuts import render, get_object_or_404, redirect
+from post.models import Post, Comments
+from post.forms import CommentForm
 
 
 def posts_list(request):
     post = Post.objects.all()
     return render(request, 'post/post_list.html', {'post': post})
 
+
 def post_card(request, pk):
-  post = get_object_or_404(Post, id=pk)
-  return render(request, 'post/post_card.html', {'post': post})
+    post = get_object_or_404(Post, id=pk)
+    comment = Comments.objects.filter(post=pk, moderation=True)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            form = form.save(commit=False)
+            form.user = request.user
+            form.post = post
+            form.save()
+            return redirect(post_card, pk)
+    else:
+        form = CommentForm()
+    return render(request, 'post/post_card.html',
+                  {'post': post,
+                   # 'comments': comment,
+                   'form': form})
