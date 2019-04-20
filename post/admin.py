5bca867b8a269a49c0dceaaf946a7c4044106157
Roman Nazarov <romnazaro@gmail.com>
2019-04-20 18:51:40 +0300

    create model comments, add summernote for edit text

--- post/admin.py
+++ post/admin.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 from post.models import *
 
 
-admin.site.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+    summernote_fields = ('text_min', 'text')
+
+
+admin.site.register(Post, PostAdmin)
 admin.site.register(Category)
 admin.site.register(Tag)
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'post', 'created', 'moderation')
+
+
+admin.site.register(Comments, CommentAdmin)
--- post/models.py
+++ post/models.py
@@ -58,3 +58,29 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Comments(models.Model):
+    user = models.ForeignKey(
+        User,
+        verbose_name='Пользователь',
+        on_delete=models.CASCADE)
+
+    post = models.ForeignKey(
+        Post,
+        verbose_name='Пост',
+        on_delete=models.CASCADE)
+
+    text = models.TextField('Комментарий')
+    created = models.DateTimeField(
+        'Дата добавления',
+        auto_now_add=True,
+        null=True)
+    moderation = models.BooleanField('Модерация', default=False)
+
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
+
+    def __str__(self):
+        return '{}'.format(self.user)
